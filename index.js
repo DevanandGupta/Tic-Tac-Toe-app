dd152f66d79496cfb4ca774a78a37c797bc0cc54
Dev Gupta <92709317+DevanandGupta@users.noreply.github.com>
2023-11-21 22:22:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const boxes = document.querySelectorAll(".box");
 const gameInfo = document.querySelector(".game-info");
-const newGameBtn = document.querySelector(".btn");
+const newGameBtn = document.querySelector(".btn"); 
 
 
 let currentPlayer;
